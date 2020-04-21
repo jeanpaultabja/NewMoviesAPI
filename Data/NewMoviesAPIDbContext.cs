718f2838367669c80a042520f348bcf45168e083
Jean Paul Tabja <jptabja@hotmail.com>
2020-04-20 22:19:05 -0500

    Update Controller

---
+++ Data/NewMoviesAPIDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NewMoviesAPI.Models;
+
+namespace NewMoviesAPI.Data
+{
+    public class NewMoviesAPIDbContext : DbContext
+    {
+        public NewMoviesAPIDbContext (DbContextOptions<NewMoviesAPIDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<NewMoviesAPI.Models.Movie> Movie { get; set; }
+    }
+}
